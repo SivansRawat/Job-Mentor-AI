53de876097039bf68a259eb9cfb266a67df03a01
Sivans <rawatsivans@gmail.com>
2026-07-19 13:18:44 +0530

    Remove variable export from use server file to fix Next.js build error

--- actions/rag.js
+++ actions/rag.js
@@ -1,7 +1,5 @@
 "use server";
 
-export const maxDuration = 60;
-
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
